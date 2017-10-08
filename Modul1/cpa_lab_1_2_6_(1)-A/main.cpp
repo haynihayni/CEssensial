528d2265f32a37c688c46f6d867c98379f4b0fbb
Max <forlivak@gmail.com>
2017-10-08 17:11:58 +0300

    Modul1

---
+++ Modul1/cpa_lab_1_2_6_(1)-A/main.cpp
@@ -0,0 +1,9 @@
+#include <iostream>
+using namespace std;
+int main(void) {
+cout << "Hello everyone who is seeing this programm now\n\n";
+cout << "\n";
+cout << "It's me, your first program.";
+cout << endl;
+return 0;
+}
---
+++ Modul1/cpa_lab_1_3_1_(1)-A/main.cpp
@@ -0,0 +1,11 @@
+#include <iostream>
+#include <iomanip>
+#include <string>
+int main()
+{
+int v=10800; // 3*60*60
+int minutes3=3*60;
+int minutes5=5*60;
+float PI=3.141526;
+std::cout << "result: " << v << std::endl;
+}
