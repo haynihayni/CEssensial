431f20f849c6831ae5b97425bbd26a38d4ea0c53
Max <forlivak@gmail.com>
2017-10-09 19:15:38 +0300

    M1.1

--- Modul1/cpa_lab_1_2_6_(1)-A/main.cpp
+++ Modul1/cpa_lab_1_2_6_(1)-A/main.cpp
@@ -2,7 +2,7 @@
 using namespace std;
 int main(void) {
 cout << "Hello everyone who is seeing this programm now\n\n";
-cout << "\n";
+    cout << "\n";
 cout << "It's me, your first program.";
 cout << endl;
 return 0;
