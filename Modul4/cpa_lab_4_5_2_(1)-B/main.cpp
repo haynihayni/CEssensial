fc8a351ea9378b4a4e048eaee43d0a1072fc6ad1
Max <forlivak@gmail.com>
2017-11-03 09:18:28 +0200

    m4 4-7

---
+++ Modul4/cpa_lab_4_5_2_(1)-B/main.cpp
@@ -0,0 +1,47 @@
+#include <iostream>
+#include <string>
+#include <stdlib.h>
+
+using namespace std;
+
+int main()
+{
+    string ip,mes="Correct IP;",t,f,l,allow="12347890.";
+    int pre=0,now=0;
+    getline(cin,ip);
+    l=ip;
+    for(int i=0;i<ip.length();++i){
+
+            if(allow.find(ip[i])==string::npos){
+            mes="Only digits and dots allowed;";
+            break;
+            }
+    }
+    for(int i=0; i<=3;++i){
+
+        now+=l.find('.');
+        if(i==3){
+            t=ip.substr(pre);
+            if(t.find('.')!=string::npos){
+                mes="Too many parts;";
+                break;
+            }
+        }
+        else{
+            t=ip.substr(pre,now);
+            l=ip.substr(now);
+        }
+
+        pre+=now+1;
+        if(t.length()>=4){
+            mes="Too many characters in a part;\n";
+            break;
+        }
+       if(atoi(t.c_str())>255){
+            mes="Too big a value of a part;";
+        }
+    }
+            cout<<mes<<endl;
+
+    return 0;
+}
---
+++ Modul4/cpa_lab_4_5_7_(1)-A/main.cpp
@@ -0,0 +1,27 @@
+#include <iostream>
+#include <string>
+using namespace std;
+
+int main()
+{
+    std::string sentence,word,rem;
+std::getline(std::cin, sentence);
+// manipulate the sentence here
+int l=0,r=sentence.find(' ');
+while(true){
+        if(r==string::npos)
+            break;
+       word = sentence.substr(l,r-l);
+       rem=sentence.substr(r+1);
+       if(rem.find(word)!=string::npos){
+            rem=rem.erase(rem.find(word),word.length());
+            sentence = sentence.substr(0,l)+word+rem;
+            r=0;
+
+       }
+       l=sentence.find(' ',r)+1;
+       r=sentence.find(' ',l);
+}
+std::cout << sentence << "\n";
+    return 0;
+}
